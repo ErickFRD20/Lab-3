63d4875f97442c93f00a34914c0abe5bf5c016ce
erickfrd <erreyesde@est.utn.ac.cr>
2026-07-23 22:17:31 -0600

    Se crean las clases dentro del paquete convertidorUnidades

--- src/covertidorUnidades/ControladorUnidades.java
+++ src/covertidorUnidades/ControladorUnidades.java
@@ -9,5 +9,13 @@ package covertidorUnidades;
  * @author mr117
  */
 public class ControladorUnidades {
-    
+    private ConvertidorUnidades modelo;
+
+    public ControladorUnidades() {
+        this.modelo = new ConvertidorUnidades();
+    }
+
+    public double convertir(double valor, EnumTemperatura origen, EnumTemperatura destino) {
+        return modelo.convertir(valor, origen, destino);
+    }
 }
